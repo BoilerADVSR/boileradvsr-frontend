71a886edb40e0f41b53b7dfe529faff87ff7a68e
Brandon Hart <brandonahart@yahoo.com>
2023-03-05 21:55:20 -0500

    Got linking to work between pages and worked on backend api calls

--- pages/SearchCourse.js
+++ pages/SearchCourse.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline, Grid } from "@mui/material";
 import theme from '../theme';
 import {Container, Paper, TextField, Button, Box, Typography, Avatar, } from "@mui/material";
+import { Link } from "react-router-dom"
 import image from './backgroundimage.jpeg'
 import { Diversity1 } from "@mui/icons-material";
 import zIndex from "@mui/material/styles/zIndex";
@@ -27,8 +28,8 @@ export default function SearchCourse() {
         <ThemeProvider theme={theme}>
         <CssBaseline/>
         <ResponsiveAppBar/>
-        <Container component="main" maxwidth="xs" alignItems="center" justifyContent="center">
-            <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" style={{ minHeight: '100vh' }}>
+        <Container component="main" maxwidth="xs" alignitems="center" justifycontent="center">
+            <Grid container spacing={0} direction="column" alignitems="center" justifycontent="center" style={{ minHeight: '100vh' }}>
                 <Grid item xs={3}>
                     <Typography color="secondary" variant="h3"><b>Find a Course</b></Typography>
                 </Grid>
